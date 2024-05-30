eb4ca6f7cda7c98c6b2d19ed4f24d03c542a7efb
fauzswalleh <swalehfauz0@gmail.com>
2024-05-30 11:07:54 +0300

    first commit

---
+++ contracts/Example contract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+contract ExampleContract {
+  mapping(address => uint256) public balances;
+
+    function setSomeonesBalance(address owner, uint256 amount) public {
+        balances[owner] = amount;
+    }
+
+    function transferTokensBetweenAddresses(address sender, address receiver, uint256 amount) public {
+        balances[sender] -= amount;   // deduct/debit the sender's balance
+        balances[receiver] += amount; // credit the reciever's balance
+    }
+      function whoami() public view returns (address,address) {
+		address sender = msg.sender;
+        address _contract = address(this);
+		return (sender,_contract);
+	}
+}
